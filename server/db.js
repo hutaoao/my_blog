40f6dc9b9cfc50ccb60742e216ed3152749bf77d
hutaoao <332413594@qq.com>
2020-05-18 17:11:08 +0800

    save

--- server/db.js
+++ server/db.js
@@ -2,9 +2,9 @@
 module.exports = {
     mysql: {
         host: 'localhost',  // 新建数据库连接时的 主机名或ID地址 内容
-        user: 'hutao',
+        user: 'root',
         password: '123456', // root 密码
         database: 'my_blog', // 数据库名
-        port: '3306'
+        port: '3305'
     }
 }
--- server/sql.js
+++ server/sql.js
@@ -1,7 +1,7 @@
 module.exports = {
     user: {
-        update: 'UPDATE users SET avatar = ? WHERE id = ?',
-        queryAll: 'SELECT * FROM users',
+        update: 'UPDATE user SET avatar = ? WHERE id = ?',
+        queryAll: 'SELECT * FROM user',
         queryUsername: 'SELECT * FROM user WHERE account = ?',
         insert: 'INSERT INTO user(account,password) VALUES (?,?)'
     },
